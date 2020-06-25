60efa124e3337ecff473c97ce07de4c7b68afdd4
Olayinka Osinubosi <olayinka@rhicstech.com>
2020-06-25 05:38:16 +0100

    Add project files.

---
+++ CompleteWaetherApp/CompleteWaetherApp/Views/CurrentWaetherPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CompleteWaetherApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CurrentWaetherPage : ContentPage
+    {
+        public CurrentWaetherPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
